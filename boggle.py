1074b12174b21fa6528b2e2ebddf6f012016522d
Luca DEttorre <ldettorre21@gmail.com>
2018-05-22 15:23:58 +0000

    function for finding neighbours of position and creating  rows/cols

--- boggle.py
+++ boggle.py
@@ -1,2 +1,29 @@
-def check():
-    return False
+from string import ascii_uppercase
+from random import choice
+
+def make_grid(cols, rows):
+    grid = {}
+    for c in range(cols):   # if cols is 5 c will be 0 to 4
+        for r in range(rows): # if rows is 3 r will be 0 to 2
+            grid[(c, r)] = choice(ascii_uppercase)
+    return grid
+    
+def get_neighbours(pos):
+    col, row = pos
+    return [
+            (col-1, row-1), 
+            (col, row-1),
+            (col+1, row-1),
+            (col+1, row),
+            (col+1, row+1),
+            (col, row+1),
+            (col-1,row+1),
+            (col-1,row),
+            ]
+            
+def all_grid_neighbours(grid):
+    all_neighbours = {}
+    for pos in grid:
+        neighbours = get_neighbours(pos)
+        all_neighbours[pos] =[pos] = neighbours
+        return all_neighbours
